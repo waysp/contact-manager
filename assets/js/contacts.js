a516a389bfd1c592506622a6843fd2d2161279bd
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2018-11-20 07:33:28 +0700

    Fix problems

--- assets/js/contacts.js
+++ assets/js/contacts.js
@@ -1,4 +1,4 @@
-/* 
+/*
 Activity: Contact manager
 */
 // TODO: Complete the program
@@ -11,59 +11,69 @@ Here`s the list of all your contacts:
 Last name: Smith, first name: John
 Last name: Doe, first name: Jane
 */
-console.log("Welcome to your contacts manager!");
-do {
-  console.log("1: List contacts");
-  console.log("2: Add a contact");
-  console.log("0: Quit");
-  var Person = {
-    //initialize the personalbar
-    init: function(last, first) {
-      this.last = last;
-      this.first = first;
-    },
-    //Describe of peoples
-    describe: function() {
-      var description =
-        "Last name: " + this.last + ", first name: " + this.first;
-      return description;
-    }
-  };
-  var person1 = Object.create(Person);
-  person1.init("Smith", "John");
-  var person2 = Object.create(Person);
-  person2.init("Doe", "Jane");
-  var person3;
-  var peoples = [];
-  peoples.push(person1);
-  peoples.push(person2);
-  function listPeople() {
-    peoples.forEach(function(people) {
-      console.log(people.describe());
-    });
+console.log('Welcome to your contacts manager!')
+
+let peoples = []
+let choice
+
+let Person = {
+  //initialize the personalbar
+  init: function(last, first) {
+    this.last = last
+    this.first = first
+  },
+  //Describe of peoples
+  describe: function() {
+    let description = 'Last name: ' + this.last + ', first name: ' + this.first
+    return description
   }
-  function addNewPerson(last, first) {
-    person3 = Object.create(Person);
-    last = prompt("Enter last name for person: ");
-    first = prompt("Enter first name for person: ");
-    person3.init(last, first);
-    var arrayPush = peoples.push(person3);
-    return arrayPush;
-  }
-  var nmbr = prompt("Enter your digit(1, 2 or 3): ");
-  var choice = parseInt(nmbr);
+}
+
+let person1 = Object.create(Person)
+person1.init('Smith', 'John')
+
+let person2 = Object.create(Person)
+person2.init('Doe', 'Jane')
+
+peoples.push(person1, person2)
+ask()
+
+function ask() {
+  console.log('1: List contacts')
+  console.log('2: Add a contact')
+  console.log('0: Quit')
+
+  const nmbr = prompt('Enter your digit(1, 2 or 0): ')
+  choice = parseInt(nmbr)
+
   switch (choice) {
     case 1:
-      console.log("Here`s the list of all your contacts: ");
-      listPeople();
-      break;
+      console.log('Here`s the list of all your contacts: ')
+      listPeople()
+      ask()
+      break
     case 2:
-      console.log("Contact added");
-      last = prompt("Enter last name for person: ");
-      first = prompt("Enter first name for person: ");
-      addNewPerson(last, first);
-      break;
+      addNewPerson()
+      console.log('Contact added')
+      ask()
+      break
     default:
-      break;
+      break
   }
-} while (choice != 0);
+}
+
+function listPeople() {
+  peoples.forEach(function(people) {
+    console.log(people.describe())
+  })
+}
+
+function addNewPerson() {
+  let newPerson = Object.create(Person)
+
+  const last = prompt('Enter last name for person: ')
+  const first = prompt('Enter first name for person: ')
+
+  newPerson.init(last, first)
+  peoples.push(newPerson)
+}
